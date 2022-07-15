e2f39ad970f591e060dfd7871e2da143c4338aa5
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-07-14 21:02:21 -0300

    Indentação em espaços

--- SOURCE/HEXAGONIX/fasmX.asm
+++ SOURCE/HEXAGONIX/fasmX.asm
@@ -33,111 +33,111 @@ fasmX:
 
 ;; Capturar os parâmetros fornecidos pelo Hexagonix®
 
-	mov [regES], es
-	
-	push ds
-	pop es			
-	
-	mov	[linha_comando], edi
-	
-	novaLinha
-	
+    mov [regES], es
+    
+    push ds
+    pop es          
+    
+    mov [linha_comando], edi
+    
+    novaLinha
+    
 ;; Início do código do FASM
 
-	mov	esi,_logo
-	call	print_string
-	
-	call	get_params
-	jc	information
+    mov esi,_logo
+    call    print_string
+    
+    call    get_params
+    jc  information
 
-	call	init_memory
+    call    init_memory
 
-	mov	esi,_memory_prefix
-	call	print_string
-	mov	eax,[memory_end]
-	sub	eax,[memory_start]
-	add	eax,[additional_memory_end]
-	sub	eax,[additional_memory]
-	shr	eax,10
-	call	display_number
-	mov	esi,_memory_suffix
-	call	print_string
+    mov esi,_memory_prefix
+    call    print_string
+    mov eax,[memory_end]
+    sub eax,[memory_start]
+    add eax,[additional_memory_end]
+    sub eax,[additional_memory]
+    shr eax,10
+    call    display_number
+    mov esi,_memory_suffix
+    call    print_string
 
-	mov	eax,78
-	mov	ebx,buffer
-	xor	ecx,ecx
-	int	0x80
-	mov	eax,dword [buffer]
-	mov	ecx,1000
-	mul	ecx
-	mov	ebx,eax
-	mov	eax,dword [buffer+4]
-	div	ecx
-	add	eax,ebx
-	mov	[start_time],eax
+    mov eax,78
+    mov ebx,buffer
+    xor ecx,ecx
+    int 0x80
+    mov eax,dword [buffer]
+    mov ecx,1000
+    mul ecx
+    mov ebx,eax
+    mov eax,dword [buffer+4]
+    div ecx
+    add eax,ebx
+    mov [start_time],eax
 
-	and	[preprocessing_done],0
-	call	preprocessor
-	or	[preprocessing_done],-1
-	call	parser
-	call	assembler
-	call	formatter
+    and [preprocessing_done],0
+    call    preprocessor
+    or  [preprocessing_done],-1
+    call    parser
+    call    assembler
+    call    formatter
 
-	call	display_user_messages
-	movzx	eax,[current_pass]
-	inc	eax
-	call	display_number
-	mov	esi,_passes_suffix
-	call	print_string
-	mov	eax,78
-	mov	ebx,buffer
-	xor	ecx,ecx
-	int	0x80
-	mov	eax,dword [buffer]
-	mov	ecx,1000
-	mul	ecx
-	mov	ebx,eax
-	mov	eax,dword [buffer+4]
-	div	ecx
-	add	eax,ebx
-	sub	eax,[start_time]
-	jnc	time_ok
-	add	eax,3600000
+    call    display_user_messages
+    movzx   eax,[current_pass]
+    inc eax
+    call    display_number
+    mov esi,_passes_suffix
+    call    print_string
+    mov eax,78
+    mov ebx,buffer
+    xor ecx,ecx
+    int 0x80
+    mov eax,dword [buffer]
+    mov ecx,1000
+    mul ecx
+    mov ebx,eax
+    mov eax,dword [buffer+4]
+    div ecx
+    add eax,ebx
+    sub eax,[start_time]
+    jnc time_ok
+    add eax,3600000
       time_ok:
-	xor	edx,edx
-	mov	ebx,100
-	div	ebx
-	or	eax,eax
-	jz	display_bytes_count
-	xor	edx,edx
-	mov	ebx,10
-	div	ebx
-	push	edx
-	call	display_number
-	mov	dl,'.'
-	call	display_character
-	pop	eax
-	call	display_number
-	mov	esi,_seconds_suffix
-	call	print_string
+    xor edx,edx
+    mov ebx,100
+    div ebx
+    or  eax,eax
+    jz  display_bytes_count
+    xor edx,edx
+    mov ebx,10
+    div ebx
+    push    edx
+    call    display_number
+    mov dl,'.'
+    call    display_character
+    pop eax
+    call    display_number
+    mov esi,_seconds_suffix
+    call    print_string
       display_bytes_count:
-	mov	eax,[written_size]
-	call	display_number
-	mov	esi,_bytes_suffix
-	call	print_string
-	xor	al,al
-	jmp	exit_program
+    mov eax,[written_size]
+    call    display_number
+    mov esi,_bytes_suffix
+    call    print_string
+    xor al,al
+    jmp exit_program
 
 information:
-	mov	esi,_usage
-	call	print_string
-	mov	al,1
-	jmp	exit_program
+    mov esi,_usage
+    call    print_string
+    mov al,1
+    jmp exit_program
 
-fileSize:	dd 0
-filePosition:	dd 0
-nomeArquivo: 	times 50 db 0
-fileNameW: 	times 50 db 0	;File name to write
+fileSize:   dd 0
+filePosition:   dd 0
+nomeArquivo:    times 50 db 0
+fileNameW:  times 50 db 0   ;File name to write
 
 include 'Hexagonix.inc' ;; Interface para o Hexagonix
 
@@ -189,6 +189,6 @@ predefinitions rb 1000h
 buffer rb 1000h
 
 
-regES:	dw 0
+regES:  dw 0
 
 endBuffer:
