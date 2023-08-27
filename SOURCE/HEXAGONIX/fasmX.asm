12f83d418efd69cf5ad3b473f72533d9228c5ec2
Felipe Lunkes <felipe.lunkes@orsegups.com.br>
2023-08-27 20:28:24 -0300

    Correção

--- SOURCE/HEXAGONIX/fasmX.asm
+++ SOURCE/HEXAGONIX/fasmX.asm
@@ -138,7 +138,7 @@ include '../VERSION.INC'
 _copyright db 'Copyright (c) 1999-2022, Tomasz Grysztar', 0xA 
            db 'Copyright (c) 2015-2022, Felipe Miguel Nery Lunkes', 0xA, 0
 
-_logo db 10, 'flat assembler for Hexagonix(R) version ',VERSION_STRING,0
+_logo db 10, 'flat assembler for Hexagonix version ',VERSION_STRING,0
 _usage db 0xA
        db 'Usage: fasmX <source> [output]',0xA
        db 0
